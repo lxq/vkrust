d642f3eb221a00cce34e969d1881ffa4929fcb3c
lxq <lxq@weetgo.com>
2019-09-25 20:18:26 +0800

    完成winit的窗口显示。

--- src/exam/00base.rs
+++ src/exam/00base.rs
@@ -1,6 +1,51 @@
+/**
+ * https://github.com/unknownue/vulkan-tutorial-rust
+ * 第一例子，对应：00_base_code.rs
+ * @author XQ Lin
+ * @email xqlin@qq.com
+ * @date 2019/09/25
+ */
 
+// winit 使用新的版本,使用方式原git例子有区别。
+use winit::{Event, EventsLoop, Window, WindowEvent, ControlFlow};
 
+// const
+const WIN_WIDTH: u32 = 800;
+const WIN_HEIGHT: u32 = 600;
+const WIN_TITLE: &'static str = "00 base";
+
+struct VkApp;
+
+impl VkApp {
+    // 初始化设置
+    fn init(el: &EventsLoop) -> Window {
+        // Window::new(el).unwrap()
+        winit::WindowBuilder::new()
+            .with_title(WIN_TITLE)
+            .with_dimensions((WIN_WIDTH, WIN_HEIGHT).into())
+            .build(el)
+            .expect("创建窗口失败")
+    }
+
+    // 主体运行函数
+    pub fn run(el: &mut EventsLoop) {
+        el.run_forever(|event |{
+            match event {
+                Event::WindowEvent {event: WindowEvent::CloseRequested, ..} => {
+                    ControlFlow::Break
+                },
+                _ => ControlFlow::Continue,
+            }
+        });
+    }
+}
 
 fn main() {
     println!("My Vulkan learning with Rust.");
+
+    let mut el = EventsLoop::new();
+    // 惯例：不需要使用的变量，以下划线开始。
+    let _win = VkApp::init(&el);
+    VkApp::run(&mut el);
+
 }
