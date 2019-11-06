8d49164476082c748a8df9f00d468256754e0756
lxq <lxq@weetgo.com>
2019-11-06 10:50:46 +0800

    自定义模块。

---
+++ src/util/consts.rs
@@ -0,0 +1,7 @@
+/**
+ * consts 常量定义模块。
+ * 
+ * @author XQ Lin
+ * @email xqlin@qq.com
+ * @date 2019/11/06 
+ */
---
+++ src/util/mod.rs
@@ -0,0 +1,9 @@
+/**
+ * util 模块主要提供了些vkRust的辅助性功能。
+ * 
+ * @author XQ Lin
+ * @email xqlin@qq.com
+ * @date 2019/10/31 
+ */
+
+pub mod consts;
