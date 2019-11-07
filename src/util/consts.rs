41b2ca3c8b86c4e4cec60e46351f82cb731f2834
lxq <lxq@weetgo.com>
2019-11-07 15:16:07 +0800

    修正错误。

--- src/util/consts.rs
+++ src/util/consts.rs
@@ -5,3 +5,9 @@
  * @email xqlin@qq.com
  * @date 2019/11/06 
  */
+
+use ash::vk_make_version;
+
+pub const APP_VERSION: u32 = vk_make_version!(1, 0, 0);
+pub const EGN_VERSION: u32 = vk_make_version!(1, 0, 0);
+pub const API_VERSION: u32 = vk_make_version!(1, 0, 92);
